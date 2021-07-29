f4313e3bdb35c36a54adcdc0c4d6d7769d68889e
ritz-293 <singhreetika.2000@gmail.com>
2021-07-29 12:47:41 +0530

    RestStep1HelloWorldServiceReetika

---
+++ restful-web-services-step1/src/main/java/com/spring/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.spring.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	
+	//GET
+	//URI - /hello-world	
+	//method - "Hello World"
+	//@RequestMapping(method=RequestMethod.GET, path = "/hello-world")
+	//OR
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+}
