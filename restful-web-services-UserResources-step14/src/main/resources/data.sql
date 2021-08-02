b808f29912505b550552b4ce45e8b86e84e49fd6
ritz-293 <singhreetika.2000@gmail.com>
2021-08-02 14:34:08 +0530

    RestStep14UserResourcesConnectionToJPAGetPostDeleteMethodReetika

---
+++ restful-web-services-UserResources-step14/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into USER values(10001, sysdate(), 'AB')
+insert into USER values(10002, sysdate(), 'Jill')
+insert into USER values(10003, sysdate(), 'Jim')
+insert into USER values(10004, sysdate(), 'Eve')
