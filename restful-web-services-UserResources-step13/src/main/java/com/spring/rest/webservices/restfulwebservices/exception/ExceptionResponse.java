ba5c551e53e246ed82e2a46616526aa92bd4213c
ritz-293 <singhreetika.2000@gmail.com>
2021-08-02 14:32:24 +0530

    RestStep13UserResourcesDifferentVersioningServicesReetika

---
+++ restful-web-services-UserResources-step13/src/main/java/com/spring/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
@@ -0,0 +1,34 @@
+package com.spring.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+	
+	//timestamp
+	private Date timestamp;
+	private String message;
+	
+	//detail
+	private String details;
+
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+	
+	
+}
