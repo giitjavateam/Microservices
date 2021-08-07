489391f783815acbb0d93a42e43ac5169342a35d
keshav <k.jha121999@gmail.com>
2021-08-07 13:42:35 +0530

    MicroservicesUsingRestTemplate

---
+++ currency-exchange-service/src/main/java/org/study/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,63 @@
+package org.study.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.stereotype.Component;
+@Entity
+public class CurrencyExchange {
+	@Id
+	private Long id;
+	@Column(name="currency_from")
+	private String from;
+	@Column(name="currency_to")
+	private String to;
+	private BigDecimal conversionMultiple;
+	private String environment;
+	
+	public CurrencyExchange() {
+	}
+	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMutiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMutiple;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+	public String getEnvironment() {
+		return environment;
+	}
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	
+
+}
